9f6692ac9eaf378842da92a724a5397a9a602af3
exwizzar <exwizzar@gmail.com>
2022-10-11 09:52:30 +1000

    Created a btn to delete a block

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import React, {useState, useCallback, selectedRow, docTolinked} from "react";
+import React, {useState} from "react";
 import WeatherBlock from "./Components/WeatherBlock"
 import './Style/appStyle.css'
 import {apiKey} from './Components/API/apiKey'
-import InputPosts from "./Components/WeatherBlock";
 
 function App() {
 
-  let a = '';
-  const[arr, setArr] = useState([])
-  const[city, setCity] = useState('')
+    const[arr, setArr] = useState([])
+    const[city, setCity] = useState('')
 
-    async function getCity() {
-     try {
-       await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${apiKey.api}`)
-       .then(res => res.json())
-       .then(result => {
-         console.log(result);
-         let newAddArr = {
-           id : result.id,
-           name : result.name,
-           weatherNum : result.main.temp,
-           weatherStr: result.weather[0].description
-         };
-         setArr([...arr, newAddArr]);
-         setCity(a)
-       });
-     } catch (error) {
-      alert('Неверное название населённого пункта, попробуйте ещё раз.')
-     }
-     
-     
+    const removePost = (arrItem) => {
+        setArr(arr.filter((item) => item.id !== arrItem.id  ))
     }
 
-  return (
-    <div className="app">
-      <div className="df">
-        <input type="text" placeholder="Город" value={city}  onChange={e => setCity(e.target.value)} />
-        <button onClick={getCity}>Добавить город</button>
-      </div>
-      <WeatherBlock arr={arr} />
-    </div>
-  );
+    async function getCity() {
+        try {
+            await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=ru&appid=${apiKey.api}`)
+            .then(res => res.json())
+            .then(result => {
+            console.log(result);
+            let newAddArr = {
+                id : result.id,
+                name : result.name,
+                weatherNum : result.main.temp,
+                weatherStr: result.weather[0].description
+            };
+            setArr([...arr, newAddArr]);
+            setCity("")
+            });
+        } catch (error) {
+            alert('Неверное название населённого пункта, попробуйте ещё раз.')
+        } 
+    }
+
+    return (
+        <div className="app">
+            <div className="df">
+                <input type="text" placeholder="Город" value={city}  onChange={e => setCity(e.target.value)} />
+                <button onClick={getCity}>Добавить город</button>
+            </div>
+            <WeatherBlock remove={removePost} arr={arr} />
+        </div>
+    );
 }
 
 export default App;
--- src/Components/WeatherBlock.jsx
+++ src/Components/WeatherBlock.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 
-const InputPosts = ({arr}) => {
+const InputPosts = ({arr, remove}) => {
 
     return (
-      <div>
-        {arr.map((item, index) => {
-          console.log(arr)
-          return (
-            <div key={index} className = 'weather-block'>
-              <p>{item.name}</p>
-              <p>{Math.ceil(item.weatherNum) - 273} °C</p>
-              <p>{item.weatherStr}</p>
-              <button >Удалить</button>
-            </div>
-          )
-          
-        })}
+        <div>
+            {arr.map((item, index) => {
+                console.log(arr)
+                return (
+                    <div key={item.id} className = 'weather-block'>
+                    <p>{item.name}</p>
+                    <p>{Math.ceil(item.weatherNum) - 273} °C</p>
+                    <p>{item.weatherStr}</p>
+                    <button onClick={() => remove(item)} >Удалить</button>
+                    </div>
+                )
+            
+            })}
       </div>
     )
 
