cd35098b98b71c9775b3c1cb649bb1400892fe3c
exwizzar <exwizzar@gmail.com>
2022-10-11 12:13:22 +1000

    add styles for interface

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {useState} from "react";
 import WeatherBlock from "./Components/WeatherBlock"
-import './Style/appStyle.css'
+import './Style/appStyle.scss'
 import {apiKey} from './Components/API/apiKey'
+import './Style/reset.scss'
 
 function App() {
 
@@ -20,9 +21,11 @@ function App() {
             console.log(result);
             let newAddArr = {
                 id : result.id,
-                name : result.name,
+                title : result.name,
                 weatherNum : result.main.temp,
-                weatherStr: result.weather[0].description
+                weatherStr: result.weather[0].description.charAt(0).toUpperCase() + result.weather[0].description.slice(1),
+                humidity : result.main.humidity,
+                icon: `http://openweathermap.org/img/w/${result.weather[0].icon}.png`
             };
             setArr([...arr, newAddArr]);
             setCity("")
@@ -32,14 +35,38 @@ function App() {
         } 
     }
 
+    const listener = event => {
+        if (event.code === "Enter" || event.code === "NumpadEnter") {
+          event.preventDefault();
+          getCity()
+        }
+      };
+      
+
     return (
         <div className="app">
-            <div className="df">
-                <input type="text" placeholder="Город" value={city}  onChange={e => setCity(e.target.value)} />
-                <button onClick={getCity}>Добавить город</button>
+            <div className="container">
+                <form className="form">
+                    <input 
+                        type="text" 
+                        placeholder="Город" 
+                        value={city}  
+                        onChange={e => setCity(e.target.value)}
+                        onKeyPress={listener}
+                        className="form__field"
+                    />
+                    <button 
+                        type="button" 
+                        onClick={getCity} 
+                        className="btn btn--primary btn--inside uppercase"
+                    >Send</button>
+                </form>
+            <WeatherBlock className='weather-block' remove={removePost} arr={arr} />
             </div>
-            <WeatherBlock remove={removePost} arr={arr} />
+
+
         </div>
+        
     );
 }
 
--- src/Components/WeatherBlock.jsx
+++ src/Components/WeatherBlock.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import classes from '../Style/weatherBlock.module.scss'
 
-const InputPosts = ({arr, remove}) => {
+
+const WeatherBlock = ({arr, remove}) => {
 
     return (
         <div>
             {arr.map((item, index) => {
                 console.log(arr)
                 return (
-                    <div key={item.id} className = 'weather-block'>
-                    <p>{item.name}</p>
-                    <p>{Math.ceil(item.weatherNum) - 273} °C</p>
-                    <p>{item.weatherStr}</p>
-                    <button onClick={() => remove(item)} >Удалить</button>
+                    <div key={item.id} className={classes.weatherBlock}>
+                        <p className={classes.title}>{item.title}</p>
+                        <p>{Math.ceil(item.weatherNum) - 273} °C</p>
+                        <div className={classes.weatherStr}>
+                            <p>{item.weatherStr}</p>
+                            <img src={item.icon} alt="weather" />
+                        </div>
+                        <p>Влажность {item.humidity}%</p>
+                        <button className={classes.button} onClick={() => remove(item)} >Удалить</button>
                     </div>
                 )
             
@@ -21,4 +27,4 @@ const InputPosts = ({arr, remove}) => {
 
 }
 
-export default InputPosts
+export default WeatherBlock
